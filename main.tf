4defd01e620fde027a28ff4432337466f1a756f7
Nima <nima_ziaee@yahoo.com>
2024-09-08 17:34:19 +0200

    my latest changes to eu-north-1

--- main.tf
+++ main.tf
@@ -8,25 +8,3 @@ resource "aws_instance" "Nima" {
     Created_by = var.created_by
   }
 }
-
-# resource "aws_instance" "Nima2" {
-#   ami           = var.ami_id
-#   instance_type = var.instance_type
-#   key_name      = var.key_name
-
-#   tags = {
-#     Name       = var.machine_name
-#     created_by = var.created_by
-#   }
-# }
-
-# module "ec2_nima1" {
-#   source        = "./module/ec2"
-#   instance_name = "Nima1"
-#   key_name      = "falconbird"
-# }
-# module "ec2_nima2" {
-#   source        = "./module/ec2"
-#   instance_name = "Nima2"
-#   key_name      = "falconbird"
-# }
--- provider.tf
+++ provider.tf
@@ -5,15 +5,6 @@ terraform {
       version = "5.16.2"
     }
   }
-
-  # This backend configuration instructs Terraform to store its state in an S3 bucket.
-  backend "s3" {
-    bucket         = "falconbirds3bucket"      # Name of the S3 bucket where the state will be stored.
-    key            = "App10/terraform.tfstate" # Path within the bucket where the state will be read/written.
-    region         = "eu-north-1"              # AWS region of the S3 bucket.
-    dynamodb_table = "terraform-lock"          # DynamoDB table used for state locking.
-    encrypt        = true                      # Ensures the state is encrypted at rest in S3.
-  }
 }
 # Configuration for the AWS provider.
 provider "aws" {
